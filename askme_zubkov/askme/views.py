2cd2321649f8abda5896a4a6b8a7d2d14e685ff7
wonderf00l <maks22036@gmail.com>
2023-04-17 16:29:08 +0300

    almost done homepage

--- askme_zubkov/askme/views.py
+++ askme_zubkov/askme/views.py
@@ -15,7 +15,9 @@ def homepage(request):
         "is_authorized" : True,
         "user_nickname" : "Mr. Pupkins",
         "profile_icon_url" : "askme/img/profile.png",
-        "avatar" : "askme/img/avatar.png"
+        "avatar" : "askme/img/avatar.png",
+        "questions" : range(20),
+        "question_content" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     }
     return render(request, 'askme/home.html', context=context) # django finds templates in 'templates' folder, therefore set path from this folder
 
