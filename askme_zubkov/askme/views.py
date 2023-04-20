1e81295782a60562ec0f8ba22af767692559a807
wonderf00l <maks22036@gmail.com>
2023-04-20 17:00:37 +0300

    1

--- askme_zubkov/askme/views.py
+++ askme_zubkov/askme/views.py
@@ -24,8 +24,20 @@ def homepage(request):
         "best_members" : best_members,
         "question_content" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     }
-    return render(request, 'askme/home.html', context=context) # django finds templates in 'templates' folder, therefore set path from this folder
+    return render(request, 'askme/index.html', context=context) # django finds templates in 'templates' folder, therefore set path from this folder
 
-
+def question_page(request, question_id):
+    tags = ["tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7"]
+    best_members = ["pupkin", "petrov", "terminator", "aligator"]
+    context = {
+        "title" : "AskMe",
+        "avatar" : "askme/img/avatar.png",
+        "questions" : range(20),
+        "tags" : tags,
+        "tags_len": range(len(tags) - 2),
+        "best_members" : best_members,
+        "question_content" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    }
+    return render(request, 'askme/question.html', context=context)
 # request - HttpRequest(query, session info)
 # HttpResponse arg (str) - HTML page's content
