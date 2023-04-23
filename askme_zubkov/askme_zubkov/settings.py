7e4af80033155675bfe456a23919fcae26539e02
wonderf00l <maks22036@gmail.com>
2023-04-23 20:12:52 +0300

    connected postgresql to django

--- askme_zubkov/askme_zubkov/settings.py
+++ askme_zubkov/askme_zubkov/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'askme_zubkov.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'exy',
+        'USER': 'exy',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
