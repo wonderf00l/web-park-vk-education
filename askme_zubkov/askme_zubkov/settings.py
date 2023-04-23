fffe15e81e59de8b5830f5be8d37c43dbe5708f4
wonderf00l <maks22036@gmail.com>
2023-04-23 10:52:42 +0300

    minor updates

--- askme_zubkov/askme_zubkov/settings.py
+++ askme_zubkov/askme_zubkov/settings.py
@@ -134,3 +134,7 @@ STATICFILES_DIRS = [] # список доп(нестандратных) путе
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
