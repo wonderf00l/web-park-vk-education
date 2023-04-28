f655a87835c1b71770d6b47c181faaf78828f491
wonderf00l <maks22036@gmail.com>
2023-04-28 13:29:13 +0300

    Подключил PostgreSQL

--- askme_zubkov/askme_zubkov/settings.py
+++ askme_zubkov/askme_zubkov/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'askme' # добавляем приложения
+    'askme',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
