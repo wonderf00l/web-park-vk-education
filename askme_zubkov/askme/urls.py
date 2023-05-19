656b2ec5e1afd7bb7f08922681f84a3e74dcffcf
wonderf00l <maks22036@gmail.com>
2023-05-20 02:26:35 +0300

    .

--- askme_zubkov/askme/urls.py
+++ askme_zubkov/askme/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', homepage, name='home'),
-    path('questions/recent/', recent_questions, name='recent'),
+    path('questions/recent/', recent_questions, name='recent_questions'),
     path('questions/hot/', hot_questions, name='hot_questions'),
     path('questions/<int:question_id>/', question_page, name='question_page'),
     path('questions/tag/<int:tag_id>/', tag_questions, name='tag_questions'),
